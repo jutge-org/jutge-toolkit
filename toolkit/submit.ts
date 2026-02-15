a62d7d80bca90a7ab80087fd052192cd5708c9fc
Jordi Petit <jordi.petit@gmail.com>
2026-02-15 11:25:54 +0100

    Minor

--- toolkit/submit.ts
+++ toolkit/submit.ts
@@ -4,11 +4,11 @@ import { submitInDirectory } from '../lib/submit'
 export const submitCmd = new Command('submit')
     .summary('Submit solutions to Jutge.org')
 
-    .argument('<sources...>', 'source files to submit (e.g. solution.cc slow.py buggy.py)')
+    .argument('<programs...>', 'programs to submit (e.g. solution.cc slow.py buggy.py)')
     .option('-d, --directory <directory>', 'problem directory', '.')
     .option('-l, --language <code>', 'language code (ca, es, en, ...)', 'en')
     .option('-n, --no-wait', 'do not wait for submissions to be judged')
 
-    .action(async (sources, { directory, language, wait }) => {
-        await submitInDirectory(directory, language, sources, !wait)
+    .action(async (programs, { directory, language, wait }) => {
+        await submitInDirectory(directory, language, programs, !wait)
     })
