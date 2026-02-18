a2b9b8a3583e2572730c8fbae807c38e27a7668a
Jordi Petit <jordi.petit@gmail.com>
2026-02-18 14:54:06 +0100

    Restrict clean to single directory

--- toolkit/clean.ts
+++ toolkit/clean.ts
@@ -2,20 +2,21 @@ import { Command, Option } from '@commander-js/extra-typings'
 import { cleanDirectory } from '../lib/cleaner'
 import tui from '../lib/tui'
 import { findRealDirectories } from '../lib/helpers'
+import { resolve } from 'path'
 
 export const cleanCmd = new Command('clean')
     .description('Clean disposable files')
 
-    .option('-d, --directories <directories...>', 'problem directories', ['.'])
+    .option('-d, --directory <directory>', 'problem directory', '.')
     .option('-a, --all', 'clean all disposable files (including generated statement and correct files', false)
     .addOption(new Option('-f, --force', 'force removal').conflicts('dryRun'))
     .addOption(new Option('-n, --dry-run', 'show but do not remove files').conflicts('force'))
 
-    .action(async ({ directories, all, force, dryRun }) => {
+    .action(async ({ directory, all, force, dryRun }) => {
         const isForce = force || false // default to dry-run if neither option is specified
 
-        await tui.section(`Cleaning generated files`, async () => {
-            const realDirectories = await findRealDirectories(directories)
+        await tui.section(`Cleaning disposable files in ${tui.hyperlink(resolve(directory))}`, async () => {
+            const realDirectories = await findRealDirectories([directory])
             for (const directory of realDirectories) {
                 await tui.section(`Cleaning directory ${tui.hyperlink(directory)}`, async () => {
                     await cleanDirectory(isForce, all, directory)
