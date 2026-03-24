0b213cbee186a0bd94fa821d6addcdd5f91ed999
Jordi Petit <jordi.petit@gmail.com>
2026-03-24 09:28:36 +0100

    Add metrics command

--- toolkit/index.ts
+++ toolkit/index.ts
@@ -25,6 +25,7 @@ import { askCmd } from './ask'
 import { convertCmd } from './convert'
 import { stageCmd } from './stage'
 import { lintCmd } from './lint'
+import { metricsCmd } from './metrics'
 import { completeInternalCmd, completionCmd } from './completion'
 import { dummiesCmd } from './dummies'
 
@@ -45,6 +46,7 @@ program.addCommand(downloadCmd)
 program.addCommand(generateCmd)
 program.addCommand(verifyCmd)
 program.addCommand(lintCmd)
+program.addCommand(metricsCmd)
 program.addCommand(submitCmd)
 program.addCommand(convertCmd)
 program.addCommand(stageCmd)
